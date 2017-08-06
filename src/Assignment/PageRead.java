691a27cda0817605e762c48a6e41dd267e8dae78
francisyzy <francisyzy>
2017-08-06 16:50:44 +0800

    Change HTML Reader

--- src/Assignment/PageRead.java
+++ src/Assignment/PageRead.java
@@ -15,28 +15,57 @@ import java.net.MalformedURLException;
 import java.net.URL;
  
 public class PageRead {
+//    public static StringBuilder readPage(String pageAddr) {
+//        try {
+//            URL url = new URL(pageAddr);
+// 
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+//          
+//            String line;
+//            StringBuilder sb=new StringBuilder();
+//            while ((line = reader.readLine()) != null) {
+//                sb.append(line+"\n");
+//            }
+// 
+//            reader.close();
+//
+//			return sb;            
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//			return new StringBuilder("");
+//        }  catch (IOException e) {
+//            e.printStackTrace();
+//			return new StringBuilder("");
+//        }
+//    }
+    
     public static StringBuilder readPage(String pageAddr) {
+        URL url;
+        InputStream is = null;
+        BufferedReader br;
+        String line;
+        StringBuilder sb = new StringBuilder();
+
         try {
-            URL url = new URL(pageAddr);
- 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-          
-            String line;
-            StringBuilder sb=new StringBuilder();
-            while ((line = reader.readLine()) != null) {
+            url = new URL(pageAddr);
+            is = url.openStream();  // throws an IOException
+            br = new BufferedReader(new InputStreamReader(is));
+
+            while ((line = br.readLine()) != null) {
                 sb.append(line+"\n");
             }
- 
-            reader.close();
-
-			return sb;            
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-			return new StringBuilder("");
-        }  catch (IOException e) {
-            e.printStackTrace();
-			return new StringBuilder("");
+        } catch (MalformedURLException mue) {
+             mue.printStackTrace();
+        } catch (IOException ioe) {
+             ioe.printStackTrace();
+        } finally {
+            try {
+                if (is != null) is.close();
+            } catch (IOException ioe) {
+                // nothing to see here
+            }
         }
+        return sb;
     }
 
 //    public static void main(String arg[]){ //testing
