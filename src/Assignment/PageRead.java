c470cd189fd114c22a55da9e7d173ea6aaa30b2f
francisyzy <francisyzy>
2017-08-06 16:52:44 +0800

    Add Google Archive

--- src/Assignment/PageRead.java
+++ src/Assignment/PageRead.java
@@ -67,6 +67,33 @@ public class PageRead {
         }
         return sb;
     }
+    
+    /*
+    Google SSL Reader Old
+    
+    String httpsURL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyCEGJG94k2ERfopUDTab03R3kQX3E8m6ok&cx=002976313549948865923:30uboh7bkhw&q=lectures";
+        URL myurl = new URL(httpsURL);
+        HttpsURLConnection con = (HttpsURLConnection)myurl.openConnection();
+        System.out.println("Connecting");
+        InputStream ins = con.getInputStream();
+        InputStreamReader isr = new InputStreamReader(ins);
+        BufferedReader in = new BufferedReader(isr);
+        
+        final Matcher googlematcher = GOOGLE_REGEX.matcher((CharSequence) in);
+        
+        String seed = "";
+        
+        if (googlematcher.find()){
+            seed = googlematcher.group(1);
+        }
+        
+        StringBuilder resulthtml = PageRead.readPage(seed);
+        
+        
+        
+        in.close();
+        
+        */
 
 //    public static void main(String arg[]){ //testing
 //
